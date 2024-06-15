7ae379a46327058b7455ec614db402e8e090e43f
Alankar <atripathi2312@gmail.com>
2024-06-15 15:10:06 +0530

    Add files via upload
    Property, setter, and deleter decorators with random module. A well-organized code to understand the fundamentals of property and setter methods.

---
+++ CIA.py
@@ -0,0 +1,31 @@
+import random
+list1 = ["Edward","Ethan"]
+list2 = ["Smith","Hunt"]
+class CIA:
+    def AgentName(self):
+        self.first = random.choice(list1)
+        self.last = random.choice(list2)
+
+    @property
+    def fullname(self):
+        return f"the full name of the agent is {self.first} {self.last}"
+
+    @fullname.setter
+    def fullname(self,name):
+        first,last = name.split()
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        del self.first,self.last
+agent = CIA()
+agent.AgentName()
+print(agent.fullname) #getter
+del agent.fullname   #deleter
+# print(agent.fullname)
+
+agent.fullname = f"{random.choice(list1)} {random.choice(list2)}" #setter
+print(agent.fullname)
+
+
